2b015bf345668466b3e8a76e5425f40cc401acca
KobeBerckmans <kobe.berckmans@student.ehb.be>
2024-04-09 16:18:40 +0200

    Initial Commit

---
+++ Werkstuk/WerkstukApp.swift
@@ -0,0 +1,17 @@
+//
+//  WerkstukApp.swift
+//  Werkstuk
+//
+//  Created by Kobe Berckmans on 09/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct WerkstukApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
